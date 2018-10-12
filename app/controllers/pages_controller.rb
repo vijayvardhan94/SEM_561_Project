6ee4d96395ff95d9323ff693704af66df377e9cc
Vijay Tadimeti <tadimetv@oregonstate.edu>
2018-10-12 15:07:02 -0700

    fixed routing errors

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def index
+  end
+
+  def login
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get  'pages/about'
+  get  'pages/login'
+  get  'pages/index'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
