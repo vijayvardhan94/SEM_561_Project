b289c69d5565c719d4063c12c43b9defffd082c0
Vijay Tadimeti <tadimetv@oregonstate.edu>
2018-10-12 15:07:02 -0700

    fixed routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   get '/about', to: 'pages#about'
   get '/login', to: 'pages#login'
   get '/', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+=======
+  get  'pages/about'
+  get  'pages/login'
+  get  'pages/index'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+>>>>>>> fixed routing errors
