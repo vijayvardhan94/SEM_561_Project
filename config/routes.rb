8083a7d45282bf296ac941b7701fee77e3d8caa2
Miguel Fernandez <miguel@10-248-60-108.wireless.oregonstate.edu>
2018-10-15 15:43:24 -0700

    Updating about page and adding unit tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/about', to: 'pages#about'
-  get '/login', to: 'pages#login'
-  get '/', to: 'pages#index'
+  get  'pages/about'
+  get  'pages/login'
+  get  'pages/index'
+  root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -16,4 +16,20 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  test "index should have signup fields" do
+    get '/'
+    assert_response :success
+    assert_select "form input[type=text][name=firstName]", 1
+    assert_select "form input[type=text][name=lastName]", 1
+    assert_select "form input[type=email][name=email]", 1
+    assert_select "form input[type=password][name=password]", 1
+    assert_select "form input[type=password][name=confirm_password]", 1
+  end
+
+  test "index should have login and about links" do
+    get '/'
+    assert_response :success
+    assert_select "a[href='/pages/login']", 1
+    assert_select "a[href='/pages/about']", 1
+  end
 end
