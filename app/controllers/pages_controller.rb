345ebe365b869a180ef16e188ddcb9eda8966fcf
Miguel Fernandez <miguel.fernandez@oregonstate.edu>
2018-10-16 14:53:05 -0700

    Adding access to FitBit data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def about
+    @hide_side_nav = true
   end
 
   def index
+    @hide_side_nav = true
   end
 
   def login
+    @hide_side_nav = true
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get  'pages/about'
   get  'pages/login'
   get  'pages/index'
+  get  'pages/dashboard'
   root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
