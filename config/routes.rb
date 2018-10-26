d9f0bc1d882fc032e8145948ab9ce6ab2d6ebc7c
dasgprat <dasguptp@oregonstate.edu>
2018-10-26 15:45:24 -0700

    Changes to Login and fixing test cases

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users' => 'pages#login', as: :user_root # creates user_root_path
+
+namespace :user do
+  root 'pages#login' # creates user_root_path
+end
+
+
   get  'pages/about'
   get  'pages/login'
   get  'pages/index'
@@ -7,4 +14,3 @@ Rails.application.routes.draw do
   root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
