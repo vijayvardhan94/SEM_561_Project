89fae98d6368166b07bfb131c2c9594c9961dd0e
dasgprat <dasguptp@oregonstate.edu>
2018-11-08 02:22:48 -0800

    Fixing login issue and other minor changes related to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   mount Dashing::Engine, at: Dashing.config.engine_path
   devise_for :users
-  get '/users' => 'pages#login', as: :user_root # creates user_root_path
+  get '/users' => 'users#profile', as: :user_root # creates user_root_path
   #post '/login' => 'pages#login' as:  :user_root
 
 namespace :user do
-  root 'pages#login' # creates user_root_path
+  root 'users#profile' # creates user_root_path
+  get 'users/sign_out' => 'users/sessions#destroy'
 end
 
 
@@ -13,7 +14,7 @@ end
   get  'pages/login'
   get  'pages/index'
   get  'pages/dashboard'
-  get  'pages/profile'
+  #get  'pages/profile'
   root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
